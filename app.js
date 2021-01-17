71bafdc9e89cfef5bee0d267031a51afdce0cc87
sandeep_git <sandeepkambham08@gmail.com>
2021-01-16 23:08:08 -0500

    Object, arrays, enum, tuple

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+// const person:{
+//     name : string;
+//     location: string;
+//     skills:string[];
+//     role:[number,string];       // to maintain fixed length strings
+// } = {
+//     name : 'sandeep',
+//     location : 'canada',
+//     skills: ['js', 'react'],
+//     role: [2, 'author'],
+// }
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "is a Admin";
+    Role[Role["READ_ONLY"] = 15] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 16] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'sandeep',
     location: 'canada',
-    skills: ['js', 'react']
+    skills: ['js', 'react'],
+    role: Role.ADMIN
 };
 console.log(person);
-for (var _i = 0, _a = person.skills; _i < _a.length; _i++) {
-    var skill = _a[_i];
-    console.log(skill.toLowerCase());
-}
 person.skills.map(function (skill) {
     console.log(skill);
 });
+// person.role = [2,"test"];
+console.log(person);
+console.log(person.role);
--- app.ts
+++ app.ts
@@ -1,15 +1,31 @@
+// const person:{
+//     name : string;
+//     location: string;
+//     skills:string[];
+//     role:[number,string];       // to maintain fixed length strings
+// } = {
+//     name : 'sandeep',
+//     location : 'canada',
+//     skills: ['js', 'react'],
+//     role: [2, 'author'],
+// }
+
+enum Role {ADMIN= "is a Admin", READ_ONLY=15, AUTHOR};
+
 const person = {
     name : 'sandeep',
     location : 'canada',
     skills: ['js', 'react'],
+    role: Role.ADMIN,
 }
 
 console.log(person);
 
-for(const skill of person.skills){
-    console.log(skill.toLowerCase());
-}
-
 person.skills.map(skill=>{
     console.log(skill);
 })
+
+// person.role = [2,"test"];
+
+console.log(person);
+console.log(person.role);
